4738c8f49f519efe59645644f68b9643d338d5f0
Rodrigo <rodrigosilvamanuel@icloud.com>
2021-02-18 14:18:38 +0000

    first commit

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rmanuel <rmanuel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 18:34:38 by rmanuel           #+#    #+#             */
-/*   Updated: 2021/02/08 18:34:39 by rmanuel          ###   ########.fr       */
+/*   Updated: 2021/02/18 14:16:17 by rmanuel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t			slen;
 	unsigned int	i;
 
-	sub = malloc(sizeof(char) * (len + 1));
+	slen = ft_strlen(s);
+	if (len > slen)
+		sub = (char *)malloc(sizeof(char) * (slen + 1));
+	else
+		sub = (char *)malloc(sizeof(char) * (len + 1));
 	if (sub == NULL)
 		return (0);
 	i = 0;
-	slen = ft_strlen(s);
 	if (!s)
 		return (0);
 	if (start < slen)
