1c0455c77c5dd806337f668378027a5c51b0fbc1
Rodrigo <rodrigosilvamanuel@icloud.com>
2021-02-18 20:35:20 +0000

    first commit

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rmanuel <rmanuel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 16:37:17 by rmanuel           #+#    #+#             */
-/*   Updated: 2021/02/17 16:37:20 by rmanuel          ###   ########.fr       */
+/*   Updated: 2021/02/18 20:34:37 by rmanuel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*s;
 	char	*d;
+	size_t	i;
 
 	s = (char *)src;
 	d = (char *)dst;
+	i = 0;
 	if (!len)
 		return (dst);
-	if (dst <= src)
+	if (src >= dst)
 		return (ft_memcpy(dst, src, len));
-	s = s + len;
-	d = d + len;
-	while (len--)
-		*--d = *--s;
+	else
+		while (i < len)
+		{
+			d[len - 1 - i] = s[len - 1 - i];
+			i++;
+		}
 	return (dst);
 }
