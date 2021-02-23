14d1b9040d954b1198f800ea6d646be294eba569
Rodrigo <rodrigosilvamanuel@icloud.com>
2021-02-23 19:07:17 +0000

    first commit

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,18 @@
 /*   By: rmanuel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 13:33:33 by rmanuel           #+#    #+#             */
-/*   Updated: 2021/02/23 14:00:48 by rmanuel          ###   ########.fr       */
+/*   Updated: 2021/02/23 19:05:39 by rmanuel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	while (*lst)
+	{
+		ft_lstdelone(*lst, del);
+		*lst = (*lst)->next;
+	}
+	lst = NULL;
+}
