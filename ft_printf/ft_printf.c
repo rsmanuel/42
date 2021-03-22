9a543cf434d68b64d7db2e9161688977c747987b
Rodrigo <r.silvamanuel42@gmail.com>
2021-03-22 21:13:49 +0000

    work with the struct

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "libft/libft.h"
-#include <stdarg.h>
+#include "ft_printf.h"
 
 void	print_c(va_list ap)
 {
@@ -17,29 +17,59 @@ void	print_d(va_list ap)
 	ft_putnbr_fd(va_arg(ap, int), 1);
 }
 
-int	format(const char *str, va_list ap)
+void parse_flags(const char *str, va_list ap, t_struct *params)
+{
+	while(ft_strchr("-+ 0#", *str)
+	{
+		if (*str == '-')
+			params->minus = 1;
+		else if (*str == '+')
+			params->plus = 1;
+		else if (*str == ' ')
+			params->space = 1;
+		else if (*str == '0')
+			params->zero = 1;
+		else if (*str == '#')
+			params->hash = 1;
+		i++;
+	}
+}
+
+void parse_modifiers(const char *str, va_list ap, t_struct *params)
+{
+
+}
+
+void print(char conversion, va_list ap, t_struct *params)
+{
+	if (conversion == 'c')
+		print_c(ap);
+	else if (conversion == 's')
+		print_s(ap);
+	else if (conversion == 'd')
+		print_d(ap);
+}
+
+int	parse_str(const char *str, va_list ap, t_struct *params)
 {
 	int len;
-	char *modifier;
+	char *conversion;
 
 	len = 0;
-	modifier = NULL;
+	conversion = NULL;
+	//parse modifiers function (deals with everything
+	//between % and the conversion symbol
 	while (ft_strchr("-. 0*#+cspdiuxX123456789", *str))
 	{
 		len++;
-		modifier = ft_strchr("cspdiuxX", *str);
-		if (modifier)
+		conversion = ft_strchr("cspdiuxX", *str);
+		if (conversion)
 			break;
 		str++;
 	}
-	if (!modifier)
+	if (!conversion)
 		return (0);
-	if (*modifier == 'c')
-		print_c(ap);
-	else if (*modifier == 's')
-		print_s(ap);
-	else if (*modifier == 'd')
-		print_d(ap);
+	print(*conversion, ap, params);
 	return (len);
 }
 
@@ -48,21 +78,24 @@ int		ft_printf(const char *fmt, ...)
 	int modifier_len;
 	int i;
 	va_list ap;
+	t_struct *params;
 	
 	i = 0;
 	va_start(ap, fmt);
+	params = (t_struct *)malloc(sizeof(t_struct));
+	reset_struct(params);
+	if (!params)
+		return (0);
 	while (fmt[i])
 	{
 		if (fmt[i] == '%')
-		{
-			modifier_len = format(&fmt[i + 1], ap);
-			i += modifier_len; 
-		}
+			i += parse_str(&fmt[i + 1], ap, params); 
 		else
 			ft_putchar_fd(fmt[i], 1);
 		i++;
 	}
 	va_end(ap);
+	free(params);
 	return (0);
 }
 
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#define CONVERSION "cspdiuxXnfge"
+#define FLAG "-+ 0#"
+#define	PRECISION ".0123456789*"
+
+#include <stdarg.h>
+#include <unistd.h>
+
+typedef struct	s_struct
+{
+	int	minus;
+	int	plus;
+	int	zero;
+	int	space;
+	int	hash;
+	int width;
+	int precision;
+}				t_struct;
+
+int		ft_printf(const char *fmt, ...);
+void	reset_struct(t_struct *params);
