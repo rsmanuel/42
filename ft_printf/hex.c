c251254baf588853bfd2ccd1f4e9acdb4cbd20c6
Rodrigo <r.silvamanuel42@gmail.com>
2021-04-13 15:41:22 +0100

    wip

--- ft_printf/hex.c
+++ ft_printf/hex.c
@@ -38,35 +38,6 @@ char *ft_itox(unsigned long nb, t_struct *params)
 	return (str);
 }
 
-void	print_x_aux(t_struct *params)
-{
-	char	*str;
-
-	str = params->str;
-	if (params->precision > -1)
-		treat_precision(params);
-	else
-	{
-		if (!params->minus && params->width && !params->zero)
-			ft_width(params, (params->width - params->len));
-		if (params->zero && params->width && !params->minus)
-		{
-			if (params->nb < 0)
-			{
-				ft_putchar_fd('-', 1);
-				params->len++;
-			}
-			ft_zero(params, (params->width - params->len));
-		}
-		if (params->nb < 0 && !params->zero)
-			ft_putchar_fd('-', 1);
-		ft_putstr_fd(str, 1);
-		params->ret += ft_strlen(str);
-		if (params->minus)
-			ft_width(params, (params->width - params->len));
-	}
-}
-
 void	print_x(va_list ap, t_struct *params)
 {
 	unsigned int nb;
@@ -75,7 +46,6 @@ void	print_x(va_list ap, t_struct *params)
 	nb = va_arg(ap, unsigned int);
 	str = ft_itox(nb, params);
 	params->str = str;
-	print_x_aux(params);
 	if (str)
 		free(str);
 }
