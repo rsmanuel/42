a720f7298a771035be217903692fd7d51c938c6f
Rodrigo <r.silvamanuel42@gmail.com>
2021-04-17 15:47:14 +0100

    wip

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -5,11 +5,13 @@ void	ft_printf_aux(int i, const char *fmt, va_list ap, t_struct *params)
 {
 	while (fmt[i])
 	{
-		if (fmt[i] == '%')
+		if (fmt[i] == '%' && fmt[i + 1] != '%')
 			i += parse_str(&fmt[i + 1], ap, params);
+		else if (fmt[i] == '%' && fmt[i + 1] == '%')
+			ft_putchar_fd('%', 1);
 		else
 		{
-			ft_putchar_fd(fmt[i], 1);
+			ft_putchar_fd(fmt[i], 1),
 			params->ret++;
 		}
 		i++;
--- ft_printf/tests/teste_x.c
+++ ft_printf/tests/teste_x.c
@@ -4,4 +4,5 @@
 
 int main(void)
 {
+    ft_printf("%%");
 }
